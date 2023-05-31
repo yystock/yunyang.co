4d0c41cae5dc131d7261d358ba53a023095b1106
yystock <yystock5299@gmail.com>
2023-05-30 22:05:06 -0400

    first commit

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://yunyang.co/sitemap.xml',
+    host: 'https://yunyang.co',
+  };
+}
